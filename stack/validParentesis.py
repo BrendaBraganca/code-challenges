c88734dc7d67d3a500a768301e4ce77dd3b32789
brenda_braganca <bragancadeassis@gmail.com>
2025-03-31 18:15:07 -0300

    feat: fix valid parentesis

--- stack/validParentesis.py
+++ stack/validParentesis.py
@@ -4,7 +4,7 @@ class Solution(object):
         for c in s:
             if(c != '}' and c != ']' and c != ')'):
                 pilha.append(c)
-            else:
+            elif(pilha):
                 if(c == '}' and pilha[-1] == '{'):
                     pilha.pop()
                 elif(c == ')' and pilha[-1] == '('):   
@@ -13,6 +13,8 @@ class Solution(object):
                     pilha.pop()
                 else:
                     return False
+            else:
+                return False
         if(len(pilha) == 0):
             return True
-        return False  
+        return False   
